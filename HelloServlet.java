f0720534e777a6e83c181e5cffe45b5b6881ddd1
lidayun1234 <70741686+lidayun1234@users.noreply.github.com>
2020-09-04 14:34:46 +0800

    fist1

---
+++ HelloServlet.java
@@ -0,0 +1,26 @@
+import com.alibaba.fastjson.JSONObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.security.URIParameter;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(urlPatterns="/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       // super.doGet(req, resp);
+        Map<String ,Object> map=new HashMap<>();
+        map.put("name","sfss");
+        String json=JSONObject.toJSONString(map);
+        resp.getWriter().print(json);
+        resp.getWriter().flush();
+        resp.getWriter().close();
+
+    }
+}
